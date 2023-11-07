500a5a353254687f6318a0a329095562caa3f818
Yur1ch <yuraboxi56@gmail.com>
2023-11-07 22:22:29 +0200

    hw24

--- 8.js
+++ 8.js
@@ -1,13 +1,20 @@
 console.log("Завдання: 8 ==============================");
 
 function task8() {
-  // Створюємо функцію fetchFakeData, яка симулює запит до сервера та повертає "дані" через Promise.resolve().
-  // Створюємо fakeData константу з данними { name: "John",age: 30}
-  // Використовуємо Promise.resolve(fakeData) для того, щоб створити вже зарезолвлений проміс.
-  // Викликаємо функцію fetchFakeData
-  // Якщо проміс виконаний успішно виводимо в консоль дані які він повертає
-  // Якщо проміс виконаний з помилкою виводимо в консоль помилку
+  function fetchFakeData() {
+    const fakeData = { name: "John", age: 30 };
+
+    return Promise.resolve(fakeData);
+  }
+
+  fetchFakeData()
+    .then((data) => {
+      console.log("Дані отримані з сервера:", data);
+    })
+    .catch((error) => {
+      console.error("Помилка при отриманні даних:", error);
+    });
 }
 
-// Викликаємо функцію task8
 task8();
+
